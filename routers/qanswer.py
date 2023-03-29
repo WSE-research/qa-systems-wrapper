b6fab0762a01eadfa6a1b881e22fb9a1a84ebe23
Aleksandr Perevalov <aleksandr.perevalov@hs-anhalt.de>
2023-03-29 15:52:28 +0200

    fixed QAnswer

--- routers/qanswer.py
+++ routers/qanswer.py
@@ -2,7 +2,7 @@ import requests
 import json
 from fastapi import APIRouter, Request
 from fastapi.responses import JSONResponse
-from routers.config import example_question, example_lang, example_kb, parse_gerbil, cache_question, find_in_cache, read_json
+from routers.config import example_question, example_lang, example_kb, parse_gerbil
 
 answer_api_url = "https://qanswer-core1.univ-st-etienne.fr/api/gerbil?query={query}&lang={lang}&kb={kb}&user=open" # fetches one (first) answer and corresponding query
 query_candidates_api_url = "https://qanswer-core1.univ-st-etienne.fr/api/qa/full?question={query}&lang={lang}&kb={kb}&user=open"
@@ -25,45 +25,41 @@ def prettify_answers(answers_raw):
 
 @router.get("/answer", description="Get answer URI")
 async def get_answer(request: Request, question: str = example_question, lang: str = example_lang, kb: str = example_kb):
-    query_data = {'query': question, 'lang': lang, 'kb': kb}
-    response = requests.post(answer_api_url, query_data).json()['questions'][0]['question'] # query to QAnswer
+    response = requests.post(answer_api_url.format(query=question, lang=lang, kb=kb)).json()
+    parsed_response = response['questions'][0]['question'] # query to QAnswer
     # preparation of the final response
     final_response = { 'answer': None, 'answers_raw': None, 'SPARQL': None, 'confidence': None}
-    final_response['answers_raw'] = json.loads(response['answers'])
+    final_response['answers_raw'] = json.loads(parsed_response['answers'])
     final_response['answer'] = prettify_answers(final_response['answers_raw'])
-    final_response['SPARQL'] = response['language'][0]['SPARQL']
-    final_response['confidence'] = response['language'][0]['confidence']
-    # cache request and response
+    final_response['SPARQL'] = parsed_response['language'][0]['SPARQL']
+    final_response['confidence'] = parsed_response['language'][0]['confidence']
+
     return JSONResponse(content=final_response)
 
 
 @router.get("/query_candidates", description="Get SPARQL query candidates")
 async def get_query_candidates(request: Request, question: str = example_question, lang: str = example_lang, kb: str = example_kb):
     response = requests.get(
-        query_candidates_api_url.format(question=question, lang=lang, kb=kb)
+        query_candidates_api_url.format(query=question, lang=lang, kb=kb)
     ).json()
     final_response = {'queries': [q['query'] for q in response['queries']]}
-    # cache request and response
+    
     return JSONResponse(content=final_response)
 
 
 @router.get("/query_candidates_raw", description="Get raw SPARQL query candidates")
 async def get_query_candidates_raw(request: Request, question: str = example_question, lang: str = example_lang, kb: str = example_kb):
     response = requests.get(
-        query_candidates_api_url.format(question=question, lang=lang, kb=kb)
+        query_candidates_api_url.format(query=question, lang=lang, kb=kb)
     ).json()
     final_response = [{'query': q['query'], 'confidence': q['confidence']} for q in response['queries']]
-    # cache request and response
+    
     return JSONResponse(content=final_response)
 
-@router.post("/gerbil_wikidata", description="Get response for GERBIL platform")
+@router.post("/gerbil_wikidata", description="Get response for GERBIL platform. Not possible to test in Swagger UI, use cURL: curl -X POST 0.0.0.0:8000/qanswer/gerbil_wikidata -d \"question=where was Angela Merkel born?&lang=en\"")
 async def get_response_for_gerbil_over_wikidata(request: Request):
     query, lang = parse_gerbil(str(await request.body()))
     print('GERBIL input:', query, lang)
-    
-    # cache = find_in_cache('qanswer_wikidata', request.url.path, query)
-    # if cache:
-        # return JSONResponse(content=cache)
         
     response = requests.post(answer_api_url.format(query=query, lang=lang, kb='wikidata')).json()['questions'][0]['question']
     final_response = {
@@ -79,20 +75,14 @@ async def get_response_for_gerbil_over_wikidata(request: Request):
             "answers": [json.loads(response['answers'])]   
         }]
     }
-    # cache request and response
-    # cache_question('qanswer_wikidata', request.url.path, query, answer_api_url, final_response)
-    ###
+    
     return JSONResponse(content=final_response)
 
 
-@router.post("/gerbil_dbpedia", description="Get response for GERBIL platform")
+@router.post("/gerbil_dbpedia", description="Get response for GERBIL platform. Not possible to test in Swagger UI, use cURL: curl -X POST 0.0.0.0:8000/qanswer/gerbil_dbpedia -d \"question=where was Angela Merkel born?&lang=en\"")
 async def get_response_for_gerbil_over_wikidata(request: Request):
     query, lang = parse_gerbil(str(await request.body()))
     print('GERBIL input:', query, lang)
-
-    # cache = find_in_cache('qanswer_dbpedia', request.url.path, query)
-    # if cache:
-        # return JSONResponse(content=cache)
         
     response = requests.post(answer_api_url.format(query=query, lang=lang, kb='dbpedia')).json()['questions'][0]['question']
     final_response = {
@@ -108,17 +98,5 @@ async def get_response_for_gerbil_over_wikidata(request: Request):
             "answers": [json.loads(response['answers'])]   
         }]
     }
-    # cache request and response
-    # cache_question('qanswer_dbpedia', request.url.path, query, answer_api_url, final_response)
-    ###
-    return JSONResponse(content=final_response)
 
-@router.post("/gerbil_answer_validation_test", description="Get response for GERBIL platform")
-async def get_response_for_gerbil_over_wikidata(request: Request):
-    query, lang = parse_gerbil(str(await request.body()))
-    query_data = {'query': query, 'lang': lang, 'kb': 'dbpedia'}
-    print('GERBIL input:', query, lang)
-        
-    final_response = read_json("routers/test.json")
-    final_response['questions'] = final_response['questions'][:1]
     return JSONResponse(content=final_response)
