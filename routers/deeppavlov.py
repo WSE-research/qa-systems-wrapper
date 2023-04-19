c929ef98157c4202a0fe3b9d0324faf25b45b863
Aleksandr Perevalov <aleksandr.perevalov@hs-anhalt.de>
2023-04-19 16:24:15 +0200

    change deeppavlov's answer structure internally

--- routers/deeppavlov.py
+++ routers/deeppavlov.py
@@ -20,7 +20,13 @@ async def get_answer(request: Request, question: str = example_question, lang: s
     response = requests.get(
         api_url.format(question=question, lang=lang)
     ).json()
-    final_response = {'answer': ['http://www.wikidata.org/entity/' + u for u in response['uri'] if u != 'NOTFOUND']}
+    
+    if len(response) > 0 and len(response[0]) > 0 and 'Q' in response[0][1]:
+        final_response = {'answer': ['http://www.wikidata.org/entity/' + response[0][1]]}
+    elif len(response) > 0 and len(response[0]) > 0 and 'Q' not in response[0][1] and not response[0][0] == 'Not Found':
+        final_response = {'answer': ['http://www.wikidata.org/entity/' + response[0][0]]}
+    else:
+        final_response = {'answer': ['NOTFOUND']}
 
     # cache request and response
     # cache_question('deeppavlov', request.url.path, question, {'question': question, 'lang': lang}, final_response)
@@ -54,8 +60,15 @@ async def get_response_for_gerbil_over_wikidata(request: Request):
         api_url.format(question=query, lang=lang), timeout=90
     ).json()
 
+    if len(response) > 0 and len(response[0]) > 0 and 'Q' in response[0][1]:
+        answer_value = 'http://www.wikidata.org/entity/' + response[0][1]
+    elif len(response) > 0 and len(response[0]) > 0 and 'Q' not in response[0][1] and not response[0][0] == 'Not Found':
+        answer_value = 'http://www.wikidata.org/entity/' + response[0][0]
+    else:
+        answer_value = 'NOTFOUND'
+
     if 'uri' in response.keys():
-        answers = [{"x": {"type": "uri", "value": 'http://www.wikidata.org/entity/' + u }} for u in response['uri'] if u != 'NOTFOUND']
+        answers = [{"x": {"type": "uri", "value": answer_value}}]
     else:
         answers = []
     
